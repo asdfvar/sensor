83a5564eb82fede8664204984eaf88b3264374a8
asdfvar <asdfvar@gmail.com>
2015-01-04 14:46:38 -0800

    Added compendium list of activities

---
+++ code/compendium.h
@@ -0,0 +1,7 @@
+/* Compendium list of physical activities from 2000 */
+
+enum activity {
+   WALKING_LVL_SLOW_FIRM  = 17152,      // walking, 2.0 mph, level, slow pace, firm serface
+   WALKING_LVL_MOD_FIRM   = 17190,      // walking, 3.0 mph, level, moderate pace, firm serface
+   BICYCLING              = 01015       // bicycling, general
+};
