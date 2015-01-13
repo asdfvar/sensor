4de38c57055783e7f7b1a795f014badf49a7ca05
asdfvar <asdfvar@gmail.com>
2015-01-12 21:37:33 -0800

    Forgot to add write_data.cpp to the repo. Needed to write the correlations to file

---
+++ code/tools/write_data.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <fstream>
+#include <iostream>
+
+namespace fio {
+ void write_correlations(const std::string path, float *corr_ax, float *corr_ay, int N) {
+
+   std::ofstream OF;
+   OF.open (path.c_str());
+
+   for (int k=0; k<N; k++) {
+      OF << corr_ax[k] << "," << corr_ay[k] << "\n";
+   }
+
+   OF.close();
+
+   std::cout << "Written correlation data to " << path << std::endl;
+
+ }
+}
