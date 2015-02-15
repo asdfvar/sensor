f6e869520875cf845d4f55bff1372de245a122bf
asdfvar <asdfvar@gmail.com>
2015-02-15 15:32:42 -0800

    Split the executable into two. One for training on the data and the other for processing using trained data.

--- code/train.cpp
+++ code/train.cpp
@@ -49,7 +49,6 @@ int main(int argc, char *argv[]) {
    float proc_time;
    int   itt = 0;
 
-
    taper_f(taper,
            time_window,
            cutoff_freq,
@@ -58,88 +57,19 @@ int main(int argc, char *argv[]) {
            dt,
            N_window);
 
-   const std::string training = "training";
-   const std::string data     = "data";
-
    /* Setup Kinetisense data */
    fio::kinIO KIN( data_path.c_str() );
 
-   matchedfilter *MF;
-   mf_list MF_activities;
+   /* Setup training data */
+   matchedfilter MF (N_window, atoi(activity_ID.c_str()));
 
-   for (int i_ref=1; i_ref<=InRefs.get_Nrefs(); i_ref++) {
-      ref_path = InRefs.get_ref_path(i_ref);
-      MF_activities.append ( new matchedfilter (ref_path.c_str(), N_window) );
-   }
+   /* Train on the data provided */
+   match_filt_training (&MF, &KIN, taper, apply_taper, samp_freq, dt, time_window,
+                        N_window, ref_time, N_ref_time, sens_training);
 
-   float ave_preproc_time = 0.0f;
-   bool initial_write = true;
+   /* Write the data to file */
+   MF.write(ref_path);
 
-   for (itt=0; KIN.valid_start_end (start_time, time_window); itt++, start_time += TIME_INC)
-   {
-
-      data_ax = KIN.get_sens_ax (start_time, 2);
-      data_ay = KIN.get_sens_ay (start_time, 2);
-      data_az = KIN.get_sens_az (start_time, 2);
-
-      for (int k=0; k<N_window+2; k++) {
-         ax[k] = data_ax[k];
-         ay[k] = data_ay[k];
-         az[k] = data_az[k];
-      }
-
-/*
- * PRE-PROCESSING
- */
-
-      gettime();
-      preproc(
-          ax,            /* Acceleration data in x               */
-          ay,            /* Acceleration data in y               */
-          az,            /* Acceleration data in z               */
-         &power,        /* Resulting power of the signal        */
-          dt,            /* Delta time comstant                  */
-          time_window,   /* Time window of the data              */
-          samp_freq,     /* Sampling frequency of the data       */
-          N_window);     /* Number of sample points              */
-      proc_time = gettime();
-      ave_preproc_time += proc_time;
-
-/*
- * MATCHED FILTER
- */
-
-      // Loop through the activities
-      for (int k=0; k<MF_activities.get_N(); k++, MF_activities.goto_next()) {
-         MF = MF_activities.get_MF();
-
-         gettime();
-         MF->run (ax, ay, dt, samp_freq, N_window, taper, apply_taper, work_buffer);
-         proc_time = gettime();
-
-         MF->write_corr ("output/correlations", tag);
-      }
-      MF_activities.goto_first();
-
-//   neuralnetwork ( ... );
-
-/*
- * ENERGY EXPENDITURE
- */
-
-      if (MF->get_corr_ax() > 0.95 && MF->get_corr_ay() > 0.95) {
-         act = WALKING_LVL_MOD_FIRM;
-      }
-
-      fio::write_val (power, "output/power"    + tag,    initial_write);
-      fio::write_val (act,   "output/activity" + tag, initial_write);
-      fio::write_val (power, "output/energy"   + tag,   initial_write);
-      initial_write = false;
-
-   }
-
-   ave_preproc_time /= (float) itt;
-   std::cout << "average pre-processing time = " << ave_preproc_time << std::endl;
 #endif
 
    delete[] ax;
