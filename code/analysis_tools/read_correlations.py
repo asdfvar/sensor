a13bd074834a93999064df9054e2b7bcbfdf96d1
asdfvar <asdfvar@gmail.com>
2015-06-19 21:21:48 -0700

    Update the python tools to read the new correlations files.

--- code/analysis_tools/read_correlations.py
+++ code/analysis_tools/read_correlations.py
@@ -13,8 +13,7 @@ class read_correlations:
       row_count = sum(1 for row in reader)
       fid.close()
 
-      self.correlations_primary   = np.zeros( row_count, dtype = float )
-      self.correlations_secondary = np.zeros( row_count, dtype = float )
+      self.correlations   = np.zeros( row_count, dtype = float )
 
       with open(path,'r') as fid:
          reader = csv.reader(fid)
@@ -22,30 +21,21 @@ class read_correlations:
          for p, row in enumerate( reader ):
             for k, data in enumerate( row ):
                if k == 0:
-                  self.correlations_primary[p] = float( data )
-               elif k == 1:
-                  self.correlations_secondary[p] = float( data )
+                  self.correlations[p] = float( data )
 
    def plot(self):
-      fig = pl.figure()
 
-      ax1 = fig.add_subplot(211)
       pl.ylim([0.0, 1.0])
       pl.title("Primary Signature Correlations")
-      ax1.plot( np.clip(self.correlations_primary, 0.0, 1.0), 'blue')
-
-      ax2 = fig.add_subplot(212)
-      pl.ylim([0.0, 1.0])
-      pl.title("Secondary Signature Correlations")
-      ax2.plot( np.clip(self.correlations_secondary, 0.0, 1.0), 'red')
+      pl.plot( np.clip(self.correlations, 0.0, 1.0), 'blue')
 
       pl.show()
 
    def print_stats(self):
       print "Primary correlation stats:"
-      print "\tmax = %f" % max(self.correlations_primary)
-      print "\tmean = %f" % np.mean(self.correlations_primary)
-      print "\tstd = %f" % np.std(self.correlations_primary)
+      print "\tmax = %f" % max(self.correlations)
+      print "\tmean = %f" % np.mean(self.correlations)
+      print "\tstd = %f" % np.std(self.correlations)
 
       print "Secondary correlation stats:"
       print "\tmax = %f" % max(self.correlations_secondary)
