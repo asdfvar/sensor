4219cb13d892b972e00bf89dc24218f6b0775c46
asdfvar <asdfvar@gmail.com>
2016-02-13 19:03:17 -0800

    Added just the right amount of memory needed in the example to run the program and also freed the memory at the end of the program.

--- mc_code/example.c
+++ mc_code/example.c
@@ -44,7 +44,7 @@ int main()
 
    float *references_x = malloc( num_ref_points * sizeof(*references_x) );
    float *references_y = malloc( num_ref_points * sizeof(*references_y) );
-   void *workspace = malloc( 22 * data_num_elements * sizeof(float) );
+   void *workspace = malloc( 26 * data_num_elements * sizeof(float) );
 
    sinwave(
        ax,
@@ -135,6 +135,13 @@ int main()
       &power,             /* Signal power                              */
       &activity,          /* Determined activity                       */
        workspace);        /* pre-allocated buffer space. Size = TBD    */
+
+   free(ax);
+   free(ay);
+   free(az);
+   free(references_x);
+   free(references_y);
+   free(workspace);
 
    return 0;
 }
