df04e6b09ce0c5f030069971aed24d934d168d0c
asdfvar <asdfvar@gmail.com>
2016-05-14 20:42:35 -0700

    Created training example code.

--- mc_code/training_example.c
+++ mc_code/training_example.c
@@ -1,5 +1,9 @@
+#include "unit_test.h"
 #include "sensor.h"
 #include <stdlib.h>
+#include <stdio.h>
+
+#define TWO_PI 6.283185307179586f
 
 int main()
 {
@@ -7,16 +11,43 @@ int main()
    const float sampling_freq    = 100.0f;
    const float data_time_length = 4.0f;
    const float total_data_time_length = 30.0f;
-   float *ax = (float*)malloc( 8000 );
-   float *ay = (float*)malloc( 8000 );
-   float *az = (float*)malloc( 8000 );
+   float *ax = (float*)malloc( 20000 );
+   float *ay = (float*)malloc( 20000 );
+   float *az = (float*)malloc( 20000 );
    const float ref_time_length = 1.5f;
-   float *reference_x = (float*)malloc( (int)(ref_time_length * sampling_freq) );
-   float *reference_y = (float*)malloc( (int)(ref_time_length * sampling_freq) );
-   const int num_samples = 40;
-   void  *workspace = malloc( 20000 );
+   float *reference_x = (float*)malloc( 600 );
+   float *reference_y = (float*)malloc( 600 );
+   const int num_samples = 4;
+   void  *workspace = malloc( 200000 );
 
-training_main(
+   /*
+   ** Example data set
+   */
+   sinwave(
+       ax,
+       TWO_PI * 4.0f, // omega
+       0.0f,          // phase
+       1.0f,          // amplitude
+       total_data_time_length,
+       sampling_freq);
+
+   sinwave(
+       ay,
+       TWO_PI * 4.0f, // omega
+       0.0f,          // phase
+       1.0f,          // amplitude
+       total_data_time_length,
+       sampling_freq);
+
+   sinwave(
+       az,
+       TWO_PI * 4.0f, // omega
+       0.0f,          // phase
+       1.0f,          // amplitude
+       total_data_time_length,
+       sampling_freq);
+
+   training_main(
          sampling_freq,          /* Sampling frequency                          */
          data_time_length,       /* data time length in seconds                 */
          total_data_time_length, /* total time length of the incoming data      */
