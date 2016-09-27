aab03aa980a134eab2b525c6a1759257e982263a
asdfvar <asdfvar@gmail.com>
2016-09-26 23:27:53 -0700

    Incremental changes.

--- notes/paper/make_flop_bar_graph.py
+++ notes/paper/make_flop_bar_graph.py
@@ -3,8 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-flops = ( 3.45, 3.42, 10.22, 5.43, 8.52, 2.98, 25.56, 3.42, 25.56, 0.71, 10.71)
-names = ("Data demeaning", "Signal power", "Covariance Matrix", "Eigen-vectors/values", "Projection onto axis", "Signal norm", "FFT", "Conjugate multiply", "Inverse FFT", "Find max correlation", "Normalizing correlation")
+flops = ( 3.02, 3.00, 8.95, 4.76, 7.46, 2.61, 22.38, 3.00, 22.38, 22.45)
+names = ("Data demeaning", "Signal power", "Covariance Matrix", "Eigen-vectors/values", "Projection onto axis", "Signal norm", "FFT", "Conjugate multiply", "Inverse FFT", "Normalization & add. proc.")
 
 fig, ax = plt.subplots()
 index = np.arange(len(flops))
@@ -12,9 +12,10 @@ width = 0.5
 opacity = 1.0
 ax.set_xticks(index + width/2.0)
 rects1 = plt.bar(index, flops, width, alpha=opacity, color='#000000')
+plt.title("Computational Cost Estimate", fontsize=26)
 plt.xlabel('')
-plt.ylabel("Measured Flops (% of Processing)", fontsize=16)
-plt.title("Processing Distribution", fontsize=26)
-plt.xticks(index + width/2.0, names, rotation=90, fontsize=14)
+plt.ylabel("Flops (% of Processing)", fontsize=16)
+plt.yticks(np.linspace(0, 25, 6), fontsize=16)
+plt.xticks(index + width/2.0, names, rotation=90, fontsize=16)
 plt.tight_layout()
 plt.savefig("flops_graph.png", bbox_inches="tight")
