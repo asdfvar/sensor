a6a0a5647bc4b02f3c3da838f608c062db653fde
asdfvar <asdfvar@gmail.com>
2016-10-24 20:49:08 -0700

    Fixed downsampling routine.

--- mc_code/preprocessing/down_sample.c
+++ mc_code/preprocessing/down_sample.c
@@ -22,12 +22,17 @@ int down_sample (float *data,
    for (k = 0; k < window ; k++) weights[k] *= norm;
 
    float val;
-   for (new_ind = 0, ind = 0; new_ind < N; ind += downsamp_fact, new_ind++)
+   for (new_ind = 0, ind = 0; new_ind < N-1; ind += downsamp_fact, new_ind++)
    {
       val = 0.0f;
       for (k = 0; k < window; k++) val += data[ind + k] * weights[k];
       data[new_ind] = val;
    }
+
+   norm = 1.0f / (0.5f * (float)(downsamp_fact * (downsamp_fact + 1)));
+   val = 0.0f;
+   for (k = 0; k < window/2; k++) val += data[ind + k] * weights[k];
+   data[new_ind] = val;
 
    return sampling_freq;
 }
